6b382095a440cdb1c43daeda3eb6a8fa97065bbf
Andrew Slade <aslade7921@yahoo.com>
2019-11-06 22:16:48 -0600

    A >600 character calculator

--- fifth-practice/source.c
+++ fifth-practice/source.c
@@ -1,9 +1,20 @@
-/* fifth practice - tiny programs */
+/* fifth practice - tiny programs - >600 char calculator*/
 #include <stdio.h>
-
-/*
- Main
-*/
-int main(void){
+#include <stdlib.h>
+int main(int argc, char *argv[]){
+ double a = 0.0, b = 0.0;char op = ' ';
+ for(int n = 1; n < argc; n++){
+  if(n % 2 == 1){ 
+   a = atof(argv[n]);
+   if(op == ' '){b = a;}else if(op == '+'){b += a;}
+   else if(op == '-'){b -= a;}else if(op == '*'){b *= a;}
+   else if(op == '/'){b /= a;}else{}
+  }
+  else{
+   if(*argv[n] == '+'){ op = '+';} else if(*argv[n] == '-'){op = '-';}
+   else if(*argv[n] == 'x'){op = '*';}else if(*argv[n] == '/'){op = '/';}
+  }
+ }
+ printf("Answer: %lf\n", b);
  return 0;
 }
