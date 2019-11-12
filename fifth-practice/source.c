ecbc036177ac2ba7cf4659e3db4b48edf29f4872
Andrew Slade <aslade7921@yahoo.com>
2019-11-12 16:27:29 -0600

    Imporved efficiency, removed legibility

--- fifth-practice/source.c
+++ fifth-practice/source.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[]){
- double a = 0.0, b = 0.0;char op = ' '; int n = 1;
+ double a = 0.0, b = 0.0;char op = ' '; int n = 1; char o[4] = {'+', '-', '*', '/'};
 for(; n < argc; n++){if(n % 2 == 1){ a = atof(argv[n]);
-   if(op == ' '){b = a;}else if(op == '+'){b += a;}else if(op == '-'){b -= a;}
-   else if(op == '*'){b *= a;}else if(op == '/'){b /= a;}else{}
-  }else{if(*argv[n] == '+'){ op = '+';} else if(*argv[n] == '-'){op = '-';}
-        else if(*argv[n] == 'x'){op = '*';}else if(*argv[n] == '/'){op = '/';}}
+   if(op == ' '){b = a;}else if(op == o[0]){b += a;}else if(op == o[1]){b -= a;}
+   else if(op == o[2]){b *= a;}else if(op == o[3]){b /= a;}else{}
+  }else{if(*argv[n] == o[0]){ op = o[0];} else if(*argv[n] == o[1]){op = o[1];}
+        else if(*argv[n] == 'x'){op = o[2];}else if(*argv[n] == o[3]){op = o[3];}}
   }printf("Answer: %lf\n", b);return 0;}
