bde29efee95e981ced1b329a90876bdba15fd0bb
Andrew Slade <aslade7921@yahoo.com>
2019-11-12 16:17:52 -0600

    Removed readability for the calculator

--- fifth-practice/source.c
+++ fifth-practice/source.c
@@ -2,19 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[]){
- double a = 0.0, b = 0.0;char op = ' ';
- for(int n = 1; n < argc; n++){
-  if(n % 2 == 1){ 
-   a = atof(argv[n]);
-   if(op == ' '){b = a;}else if(op == '+'){b += a;}
-   else if(op == '-'){b -= a;}else if(op == '*'){b *= a;}
-   else if(op == '/'){b /= a;}else{}
-  }
-  else{
-   if(*argv[n] == '+'){ op = '+';} else if(*argv[n] == '-'){op = '-';}
-   else if(*argv[n] == 'x'){op = '*';}else if(*argv[n] == '/'){op = '/';}
-  }
- }
- printf("Answer: %lf\n", b);
- return 0;
-}
+ double a = 0.0, b = 0.0;char op = ' '; int n = 1;
+for(; n < argc; n++){if(n % 2 == 1){ a = atof(argv[n]);
+   if(op == ' '){b = a;}else if(op == '+'){b += a;}else if(op == '-'){b -= a;}
+   else if(op == '*'){b *= a;}else if(op == '/'){b /= a;}else{}
+  }else{if(*argv[n] == '+'){ op = '+';} else if(*argv[n] == '-'){op = '-';}
+        else if(*argv[n] == 'x'){op = '*';}else if(*argv[n] == '/'){op = '/';}}
+  }printf("Answer: %lf\n", b);return 0;}
