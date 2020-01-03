a23cf55218c4077d7bec78b033bc24c87288fb8e
Andrew Slade <aslade7921@yahoo.com>
2020-01-03 17:58:51 -0600

    Added my first experiment

--- interpreter/parser.c
+++ interpreter/parser.c
@@ -1,6 +1,13 @@
 #include "interpreter.h"
 #include <stdio.h>
 
+void parse_io(FILE); 
+
 void parse(FILE *filepointer){
   printf("Parsing file...\n");
+  parse_io(filepointer);
+}
+
+void parse_io(FILE *file){
+
 }
